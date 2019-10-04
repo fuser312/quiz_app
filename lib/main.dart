6e27907a23f19abe835ad9ee77684fd7933e29a3
fuser <sahil.dogstar@gmail.com>
2019-10-04 14:48:58 +0530

    FInished Quiz App Repo.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,7 +20,54 @@ class _QuizAppState extends State<QuizApp> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Text("Your content here"),
+      child: Scaffold(
+        backgroundColor: Colors.black87,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(8, 8, 8, 144),
+                  child: Text(
+                      "This is where the Question Text will go", textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+
+
+                      ),
+                  ),
+
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: Card(
+                  color: Colors.green,
+                  child: FlatButton(
+                    child: Text(
+                      "True",
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: Card(
+                  color: Colors.red,
+                  child: FlatButton(
+                    child: Text(
+                      "False",
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
